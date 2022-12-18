cd6f9ca900cb241f49e65b5546bee998d99f29a8
Neo103SB <114983894+Neo103SB@users.noreply.github.com>
2022-12-18 20:28:19 +0100

    Update Python_streamlit.py

--- Python_streamlit.py
+++ Python_streamlit.py
@@ -88,195 +88,7 @@ st.write("Total Amount:", sum_value, format= 'plain')
 
 #############################################GRAPHES_CREATION#######################################
 ######FIRST
-l1 = []
-for x in df['Rappel du partenaire technologique']:
-    if x not in l1:
-        l1.append(x)
-l2 = []
-v1 = 0
-v2 = 0
-v3 = 0
-v4 = 0
-v5 = 0
-v6 = 0
-v7 = 0
-v8 = 0
-v9 = 0
-v10 = 0
-v11 = 0
-v12 = 0
-v13 = 0
-v14 = 0
-v15 = 0
-v16 = 0
-v17 = 0
-for i in range(len(df['Rappel du partenaire technologique'])):
-    if df['Rappel du partenaire technologique'][i] == 'DELL EMC':
-        v1 = v1 + df[' "Amount"'][i]
-    if df['Rappel du partenaire technologique'][i] == 'NaN':
-        V2 = v2 + df[' "Amount"'][i]
-    if df['Rappel du partenaire technologique'][i] == 'VEEAM':
-        v3 = v3 + df[' "Amount"'][i]
-    if df['Rappel du partenaire technologique'][i] == 'HUAWEI':
-        v4 = v4 + df[' "Amount"'][i]
-    if df['Rappel du partenaire technologique'][i] == 'JIRA':
-        v5 = v5 + df[' "Amount"'][i]
-    if df['Rappel du partenaire technologique'][i] == 'IPC':
-        v6 = v6 + df[' "Amount"'][i]
-    if df['Rappel du partenaire technologique'][i] == 'CISCO':
-        v7 = v7 + df[' "Amount"'][i]
-    if df['Rappel du partenaire technologique'][i] == 'FORTINET':
-        v8 = v8 + df[' "Amount"'][i]
-    if df['Rappel du partenaire technologique'][i] == 'CHECK POINT':
-        v9 = v9 + df[' "Amount"'][i]
-    if df['Rappel du partenaire technologique'][i] == 'Symantec':
-        v10 = v10 + df[' "Amount"'][i]
-    if df['Rappel du partenaire technologique'][i] == 'Trend Micro':
-        v11 = v11 + df[' "Amount"'][i]
-    if df['Rappel du partenaire technologique'][i] == 'Beyondtrust':
-        v12 = v12 + df[' "Amount"'][i]
-    if df['Rappel du partenaire technologique'][i] == 'Dynatrace':
-        v13 = v13 + df[' "Amount"'][i]
-    if df['Rappel du partenaire technologique'][i] == 'Dell':
-        v14 = v14 + df[' "Amount"'][i]
-    if df['Rappel du partenaire technologique'][i] == 'Microsoft':
-        v15 = v15 + df[' "Amount"'][i]
-    if df['Rappel du partenaire technologique'][i] == 'Solarwinds':
-        v16 = v16 + df[' "Amount"'][i]
-    if df['Rappel du partenaire technologique'][i] == 'DELL EMC':
-        v17 = v17 + df[' "Amount"'][i]
-
-l2.append(v1)
-l2.append(v2)
-l2.append(v3)
-l2.append(v4)
-l2.append(v5)
-l2.append(v6)
-l2.append(v7)
-l2.append(v8)
-l2.append(v9)
-l2.append(v10)
-l2.append(v11)
-l2.append(v12)
-l2.append(v13)
-l2.append(v14)
-l2.append(v15)
-l2.append(v16)
-l2.append(v17)
-
-d = []
-
-for i in range(len(l1)):
-    d.append([l1[i], l2[i]])
-
-###SECOND:
-
-m1=[]
-for x in df[' "Solution"']:
-  if x not in m1 :
-    m1.append(x)
-m2=[]
-w1 = 0
-w2= 0
-w3 = 0
-w4 = 0
-w5 = 0
-w6 = 0
-w7 = 0
-w8 = 0
-w9 = 0
-w10 = 0
-w11 = 0
-w12 = 0
-w13 = 0
-for i in range(len(df[' "Solution"'])):
-  if df[' "Solution"'][i] == 'Cloud':
-    w1 = w1 + df[' "Amount"'][i]
-  if df[' "Solution"'][i] == 'Postes de trawail':
-    w2 = w2 + df[' "Amount"'][i]
-  if df[' "Solution"'][i] == 'Réseau':
-    w3 = w3 + df[' "Amount"'][i]
-  if df[' "Solution"'][i] == 'Régie INEOS':
-     w4 = w4 + df[' "Amount"'][i]
-  if df[' "Solution"'][i] == 'Sécurité':
-    w5 = w5 + df[' "Amount"'][i]
-  if df[' "Solution"'][i] == 'Firewalling':
-    w6 = w6 + df[' "Amount"'][i]
-  if df[' "Solution"'][i] == 'Régie Cyberforce':
-    w7 = w7 + df[' "Amount"'][i]
-  if df[' "Solution"'][i] == 'Sécurité Poste de Travail':
-    w8 = w8 + df[' "Amount"'][i]
-  if df[' "Solution"'][i] == 'Audit et conseil':
-    w9 = w9 + df[' "Amount"'][i]
-  if df[' "Solution"'][i] == 'SOC':
-    w10 = w10 + df[' "Amount"'][i]
-  if df[' "Solution"'][i] == 'Maintenance':
-    w11 = w11 + df[' "Amount"'][i]
-  if df[' "Solution"'][i] == 'DevOps':
-    w12 = w12 + df[' "Amount"'][i]
-  if df[' "Solution"'][i] == 'NaN':
-    w13 = w13 + df[' "Amount"'][i]
-m2.append(w1)
-m2.append(w2)
-m2.append(w3)
-m2.append(w4)
-m2.append(w5)
-m2.append(w6)
-m2.append(w7)
-m2.append(w8)
-m2.append(w9)
-m2.append(w10)
-m2.append(w11)
-m2.append(w12)
-m2.append(w13)
-
-z=[]
-for i in range(len(m1)):
-  z.append([m1[i],m2[i]])
-st.header("BI APP TEST")
-st.write("VERSION BETA DE L'APP")
-
-######THIRD
-
-b1=[]
-for x in df[' "Assigned To"']:
-  if x not in b1 :
-    b1.append(x)
-b2=[]
-u1 = 0
-u2= 0
-u3 = 0
-u4 = 0
-u5 = 0
-u6 = 0
-for i in range(len(df[' "Assigned To"'])):
-  if df[' "Assigned To"'][i] == 'reda bakkali' :
-    u1 = u1 + df[' "Amount"'][i]
-  if df[' "Assigned To"'][i] ==  'amine nemrouch' :
-    u2 = u2 + df[' "Amount"'][i]
-  if df[' "Assigned To"'][i] == 'mohamed ikniouen':
-    u3 = u3 + df[' "Amount"'][i]
-  if df[' "Assigned To"'][i] == 'iidriss zaamoun':
-    u4 = u4 + df[' "Amount"'][i]
-  if df[' "Assigned To"'][i] == 'hamida benlemlih':
-    u5 = u5 + df[' "Amount"'][i]
-  if df[' "Assigned To"'][i] ==  'yahya ajbali':
-    u6 = u6 + df[' "Amount"'][i]
-
-b2.append(u1)
-b2.append(u2)
-b2.append(u3)
-b2.append(u4)
-b2.append(u5)
-b2.append(u6)
-
-c=[]
-
-for i in range(len(b1)):
-  c.append([b1[i],b2[i]])
-
-######FOURTH:
-
+########graphe 1 :
 Subject = ['Qualification', 'Closed Won', 'Nouveau', 'Closed Lost', 'Proposition', 'Abandonnée']
 a1=0
 a2=0
@@ -315,15 +127,281 @@ for i in range(len(df[' "Entité"'])):
     a11 = a11 + df[' "Amount"'][i]
   if df[' "Entité"'][i] == 'Cyberforce' and df[' "Sales Stage"'][i] == 'Abandonnée':
     a12 = a12 + df[' "Amount"'][i]
-
 # Create the pandas DataFrame
-df = pd.DataFrame([['Qualification', 'Ineos',a1],['Qualification', 'Cyberforce',a2],['Closed Won', 'Ineos',a3],['Closed Won', 'Cyberforce',a4],
+df2 = pd.DataFrame([['Qualification', 'Ineos',a1],['Qualification', 'Cyberforce',a2],['Closed Won', 'Ineos',a3],['Closed Won', 'Cyberforce',a4],
                    ['Nouveau', 'Ineos',a5],['Nouveau', 'Cyberforce',a6],['Closed Lost', 'Ineos',a7],['Closed Lost', 'Cyberforce',a8],
                    ['Proposition', 'Ineos',a9],['Proposition', 'Cyberforce',a10],['Abandonnée', 'Ineos',a11],['Abandonnée', 'Cyberforce',a12]
                    ], columns = ['Opportunities', 'Entité','Sales'])
-fig = px.bar(df, x="Opportunities", y="Sales", color="Entité",barmode='group')
-fig.show()
+fig1 = px.bar(df2, x="Opportunities", y="Sales", color="Entité",barmode='group')
+st.plotly_chart(fig1)
 
+###############graphe 2 :
+r1 = 0
+r2 =0 
+r3 =0
+r4 =0
+r5 =0
+r6 =0
+
+b1=0
+b2=0
+b3=0
+b4=0
+b5=0
+b6=0
+
+m1=0
+m2=0
+m3=0
+m4=0
+m5=0
+m6=0
+
+i1=0
+i2=0
+i3=0
+i4=0
+i5=0
+i6=0
+
+h1=0
+h2=0
+h3=0
+h4=0
+h5=0
+h6=0
+
+y1=0
+y2=0
+y3=0
+y4=0
+y5=0
+y6=0
+
+for j in range(len(df[' "Assigned To"'])):
+  if df[' "Assigned To"'][j] == 'reda.bakkali' and df[' "Sales Stage"'][j] == 'Qualification' and df[' "Amount"'][j] !=0 :
+    r1 = r1 + 1
+  if df[' "Assigned To"'][j] == 'reda.bakkali' and df[' "Sales Stage"'][j] == 'Closed Won' and df[' "Amount"'][j] !=0 :
+    r2 = r2 + 1
+  if df[' "Assigned To"'][j] == 'reda.bakkali' and df[' "Sales Stage"'][j] == 'Nouveau' and df[' "Amount"'][j] !=0 :
+     r3 = r3 + 1
+  if df[' "Assigned To"'][j] == 'reda.bakkali' and df[' "Sales Stage"'][j] == 'Closed Lost' and df[' "Amount"'][j] !=0 :
+     r4 = r4 + 1
+  if df[' "Assigned To"'][j] == 'reda.bakkali' and df[' "Sales Stage"'][j] == 'Proposition' and df[' "Amount"'][j] !=0 :
+     r5 = r5 + 1
+  if df[' "Assigned To"'][j] == 'reda.bakkali' and df[' "Sales Stage"'][j] == 'Abandonnée' and df[' "Amount"'][j] !=0 :
+     r6 = r6 + 1
+  
+  if df[' "Assigned To"'][j] == 'amine.nemrouch' and df[' "Sales Stage"'][j] == 'Qualification' and df[' "Amount"'][j] !=0 :
+    b1 = b1 + 1
+  if df[' "Assigned To"'][j] == 'amine.nemrouch' and df[' "Sales Stage"'][j] == 'Closed Won' and df[' "Amount"'][j] !=0 :
+    b2 = b2 + 1
+  if df[' "Assigned To"'][j] == 'amine.nemrouch' and df[' "Sales Stage"'][j] == 'Nouveau' and df[' "Amount"'][j] !=0 :
+     b3 = b3 + 1
+  if df[' "Assigned To"'][j] == 'amine.nemrouch' and df[' "Sales Stage"'][j] == 'Closed Lost' and df[' "Amount"'][j] !=0 :
+     b4 = b4 + 1
+  if df[' "Assigned To"'][j] == 'amine.nemrouch' and df[' "Sales Stage"'][j] == 'Proposition' and df[' "Amount"'][j] !=0 :
+     b5 = b5 + 1
+  if df[' "Assigned To"'][j] == 'amine.nemrouch' and df[' "Sales Stage"'][j] == 'Abandonnée' and df[' "Amount"'][j] !=0 :
+     b6 = b6 + 1
+
+  if df[' "Assigned To"'][j] == 'mohamed.ikniouen' and df[' "Sales Stage"'][j] == 'Qualification' and df[' "Amount"'][j] !=0 :
+    m1 = m1 + 1
+  if df[' "Assigned To"'][j] == 'mohamed.ikniouen' and df[' "Sales Stage"'][j] == 'Closed Won' and df[' "Amount"'][j] !=0 :
+    m2 = m2 + 1
+  if df[' "Assigned To"'][j] == 'mohamed.ikniouen' and df[' "Sales Stage"'][j] == 'Nouveau' and df[' "Amount"'][j] !=0 :
+     m3 = m3 + 1
+  if df[' "Assigned To"'][j] == 'mohamed.ikniouen' and df[' "Sales Stage"'][j] == 'Closed Lost' and df[' "Amount"'][j] !=0 :
+     m4 = m4 + 1
+  if df[' "Assigned To"'][j] == 'mohamed.ikniouen' and df[' "Sales Stage"'][j] == 'Proposition' and df[' "Amount"'][j] !=0 :
+     m5 = m5 + 1
+  if df[' "Assigned To"'][j] == 'mohamed.ikniouen' and df[' "Sales Stage"'][j] == 'Abandonnée' and df[' "Amount"'][j] !=0 :
+     m6 = m6 + 1
+
+  if df[' "Assigned To"'][j] == 'iidriss.zaamoun' and df[' "Sales Stage"'][j] == 'Qualification' and df[' "Amount"'][j] !=0 :
+    i1 = i1 + 1
+  if df[' "Assigned To"'][j] == 'iidriss.zaamoun' and df[' "Sales Stage"'][j] == 'Closed Won' and df[' "Amount"'][j] !=0 :
+    i2 = i2 + 1
+  if df[' "Assigned To"'][j] == 'iidriss.zaamoun' and df[' "Sales Stage"'][j] == 'Nouveau' and df[' "Amount"'][j] !=0 :
+     i3 = i3 + 1
+  if df[' "Assigned To"'][j] == 'iidriss.zaamoun' and df[' "Sales Stage"'][j] == 'Closed Lost' and df[' "Amount"'][j] !=0 :
+     i4 = i4 + 1
+  if df[' "Assigned To"'][j] == 'iidriss.zaamoun' and df[' "Sales Stage"'][j] == 'Proposition' and df[' "Amount"'][j] !=0 :
+     i5 = i5 + 1
+  if df[' "Assigned To"'][j] == 'iidriss.zaamoun' and df[' "Sales Stage"'][j] == 'Abandonnée' and df[' "Amount"'][j] !=0 :
+     i6 = i6 + 1
+
+  if df[' "Assigned To"'][j] == 'hamida.benlemlih' and df[' "Sales Stage"'][j] == 'Qualification' and df[' "Amount"'][j] !=0 :
+    h1 = h1 + 1
+  if df[' "Assigned To"'][j] == 'hamida.benlemlih' and df[' "Sales Stage"'][j] == 'Closed Won' and df[' "Amount"'][j] !=0 :
+    h2 = h2 + 1
+  if df[' "Assigned To"'][j] == 'hamida.benlemlih' and df[' "Sales Stage"'][j] == 'Nouveau' and df[' "Amount"'][j] !=0 :
+     h3 = h3 + 1
+  if df[' "Assigned To"'][j] == 'hamida.benlemlih' and df[' "Sales Stage"'][j] == 'Closed Lost' and df[' "Amount"'][j] !=0 :
+     h4 = h4 + 1
+  if df[' "Assigned To"'][j] == 'hamida.benlemlih' and df[' "Sales Stage"'][j] == 'Proposition' and df[' "Amount"'][j] !=0 :
+     h5 = h5 + 1
+  if df[' "Assigned To"'][j] == 'hamida.benlemlih' and df[' "Sales Stage"'][j] == 'Abandonnée' and df[' "Amount"'][j] !=0 :
+     h6 = h6 + 1
+
+  if df[' "Assigned To"'][j] == 'yahya.ajbali' and df[' "Sales Stage"'][j] == 'Qualification' and df[' "Amount"'][j] !=0 :
+    y1 = y1 + 1
+  if df[' "Assigned To"'][j] == 'yahya.ajbali' and df[' "Sales Stage"'][j] == 'Closed Won' and df[' "Amount"'][j] !=0 :
+    y2 = y2 + 1
+  if df[' "Assigned To"'][j] == 'yahya.ajbali' and df[' "Sales Stage"'][j] == 'Nouveau' and df[' "Amount"'][j] !=0 :
+     y3 = y3 + 1
+  if df[' "Assigned To"'][j] == 'yahya.ajbali' and df[' "Sales Stage"'][j] == 'Closed Lost' and df[' "Amount"'][j] !=0 :
+     y4 = y4 + 1
+  if df[' "Assigned To"'][j] == 'yahya.ajbali' and df[' "Sales Stage"'][j] == 'Proposition' and df[' "Amount"'][j] !=0 :
+     y5 = y5 + 1
+  if df[' "Assigned To"'][j] == 'yahya.ajbali' and df[' "Sales Stage"'][j] == 'Abandonnée' and df[' "Amount"'][j] !=0 :
+     y6 = y6 + 1
+
+df3 = pd.DataFrame([['Qualification', 'reda.bakkali',r1],['Closed Won', 'reda.bakkali',r2],['Nouveau', 'reda.bakkali',r3],['Closed Lost', 'reda.bakkali',r4],['Proposition', 'reda.bakkali',r5],['Abandonnée', 'reda.bakkali',r6],
+                   ['Qualification', 'amine.nemrouch',b1],['Closed Won', 'amine.nemrouch',b2],['Nouveau', 'amine.nemrouch',b3],['Closed Lost', 'amine.nemrouch',b4],['Proposition', 'amine.nemrouch',b5],['Abandonnée', 'amine.nemrouch',b6],
+                   ['Qualification', 'mohamed.ikniouen',m1],['Closed Won', 'mohamed.ikniouen',m2],['Nouveau', 'mohamed.ikniouen',m3],['Closed Lost', 'mohamed.ikniouen',m4],['Proposition', 'mohamed.ikniouen',m5],['Abandonnée', 'mohamed.ikniouen',m6],
+                   ['Qualification', 'iidriss.zaamoun',i1],['Closed Won', 'iidriss.zaamoun',i2],['Nouveau', 'iidriss.zaamoun',i3],['Closed Lost', 'iidriss.zaamoun',i4],['Proposition', 'iidriss.zaamoun',i5],['Abandonnée', 'iidriss.zaamoun',i6],
+                   ['Qualification', 'hamida.benlemlih',h1],['Closed Won', 'hamida.benlemlih',h2],['Nouveau', 'hamida.benlemlih',h3],['Closed Lost', 'hamida.benlemlih',h4],['Proposition', 'hamida.benlemlih',h5],['Abandonnée', 'hamida.benlemlih',h6],
+                   ['Qualification', 'yahya.ajbali',y1],['Closed Won', 'yahya.ajbali',y2],['Nouveau', 'yahya.ajbali',y3],['Closed Lost', 'yahya.ajbali',y4],['Proposition', 'yahya.ajbali',y5],['Abandonnée', 'yahya.ajbali',y6]
+                   ], columns = ['Sales Stage', 'Manager','Sales'])
+
+fig2 = px.bar(df3, x='Sales Stage', y="Sales", color='Manager',barmode='group')
+st.plotly_chart(fig2)
+
+#############graphe 3 :
+v1 = 0
+v2= 0
+v3 = 0
+v4 = 0
+v5 = 0
+v6 = 0
+for i in range(len(df[' "Assigned To"'])):
+  if df[' "Assigned To"'][i] == 'reda.bakkali' :
+    v1 = v1 + df[' "Amount"'][i]
+  if df[' "Assigned To"'][i] ==  'amine.nemrouch'	:
+    v2 = v2 + df[' "Amount"'][i]
+  if df[' "Assigned To"'][i] == 'mohamed.ikniouen':
+    v3 = v3 + df[' "Amount"'][i]
+  if df[' "Assigned To"'][i] == 'iidriss.zaamoun':
+     v4 = v4 + df[' "Amount"'][i]
+  if df[' "Assigned To"'][i] == 'hamida.benlemlih':
+    v5 = v5 + df[' "Amount"'][i]
+  if df[' "Assigned To"'][i] ==  'yahya.ajbali':
+    v6 = v6 + df[' "Amount"'][i]
+
+df4 = pd.DataFrame([['reda bakkali', v1],['amine nemrouch',v2],['mohamed ikniouen', v3],['iidriss zaamoun', v4],
+                   ['hamida benlemlih',v5],['yahya ajbali',v6],
+                   ], columns = ['Manager','Sales'])
+fig3 = px.bar(df4, x="Manager", y="Sales")
+st.plotly_chart(fig3)
+
+################graphe 4 :
+w1 = 0
+w2= 0
+w3 = 0
+w4 = 0
+w5 = 0
+w6 = 0
+w7 = 0
+w8 = 0
+w9 = 0
+w10 = 0
+w11 = 0
+w12 = 0  
+w13 = 0
+w14 = 0
+w15 = 0
+w16 = 0
+w17 = 0
+for i in range(len(df['Rappel du partenaire technologique'])):
+  if df['Rappel du partenaire technologique'][i] == 'DELL EMC':
+    w1 = w1 + df[' "Amount"'][i]
+  if df['Rappel du partenaire technologique'][i] == 'NaN':
+    w2 = w2 + df[' "Amount"'][i]
+  if df['Rappel du partenaire technologique'][i] == 'VEEAM':
+    w3 = w3 + df[' "Amount"'][i]
+  if df['Rappel du partenaire technologique'][i] == 'HUAWEI':
+     w4 = w4 + df[' "Amount"'][i]
+  if df['Rappel du partenaire technologique'][i] == 'JIRA':
+    w5 = w5 + df[' "Amount"'][i]
+  if df['Rappel du partenaire technologique'][i] == 'IPC':
+    w6 = w6 + df[' "Amount"'][i]
+  if df['Rappel du partenaire technologique'][i] == 'CISCO':
+    w7 = w7 + df[' "Amount"'][i]
+  if df['Rappel du partenaire technologique'][i] == 'FORTINET':
+    w8 = w8 + df[' "Amount"'][i]
+  if df['Rappel du partenaire technologique'][i] == 'CHECK POINT':
+    w9 = w9 + df[' "Amount"'][i]
+  if df['Rappel du partenaire technologique'][i] == 'Symantec':
+    w10 = w10 + df[' "Amount"'][i]
+  if df['Rappel du partenaire technologique'][i] == 'Trend Micro':
+    w11 = w11 + df[' "Amount"'][i]
+  if df['Rappel du partenaire technologique'][i] == 'Beyondtrust':
+    w12 = w12 + df[' "Amount"'][i]
+  if df['Rappel du partenaire technologique'][i] == 'Dynatrace':
+    w13 = w13 + df[' "Amount"'][i]
+  if df['Rappel du partenaire technologique'][i] == 'Dell':
+    w14 = w14 + df[' "Amount"'][i]
+  if df['Rappel du partenaire technologique'][i] == 'Microsoft':
+    w15 = w15 + df[' "Amount"'][i]
+  if df['Rappel du partenaire technologique'][i] == 'Solarwinds':
+    w16 = w16 + df[' "Amount"'][i]
+  if df['Rappel du partenaire technologique'][i] == 'DELL EMC':
+    w17 = w17 + df[' "Amount"'][i]
+  
+df5 = pd.DataFrame([['DELL EMC', w1],['NaN',w2],['VEEAM', w3],['HUAWEI', w4],['JIRA', w5],['IPC',w6],['CISCO', w7],['FORTINET', w8],
+                   ['CHECK POINT', w9],['Symantec',w10],['Trend Micro', w11],['Beyondtrust', w12],['Dynatrace', w12],['Dell',w13],['Microsoft', w14],['Solarwinds', w15],
+                    [ 'DELL EMC',w16]
+                   ], columns = ['partenaire technologique','Sales'])
+fig4 = px.bar(df5, x="partenaire technologique", y="Sales")
+st.plotly_chart(fig4)
+
+############graphe 5 :
+s1 = 0
+s2= 0
+s3 = 0
+s4 = 0
+s5 = 0
+s6 = 0
+s7 = 0
+s8 = 0
+s9 = 0
+s10 = 0
+s11 = 0
+s12 = 0  
+s13 = 0
+for i in range(len(df[' "Solution"'])):
+  if df[' "Solution"'][i] == 'Cloud':
+    s1 = s1 + df[' "Amount"'][i]
+  if df[' "Solution"'][i] == 'Postes de travail':
+    s2 = s2 + df[' "Amount"'][i]
+  if df[' "Solution"'][i] == 'Réseau':
+    s3 = s3 + df[' "Amount"'][i]
+  if df[' "Solution"'][i] == 'Régie INEOS':
+     s4 = s4 + df[' "Amount"'][i]
+  if df[' "Solution"'][i] == 'Sécurité':
+    s5 = s5 + df[' "Amount"'][i]
+  if df[' "Solution"'][i] == 'Firewalling':
+    s6 = s6 + df[' "Amount"'][i]
+  if df[' "Solution"'][i] == 'Régie Cyberforce':
+    s7 = s7 + df[' "Amount"'][i]
+  if df[' "Solution"'][i] == 'Sécurité Poste de Travail':
+    s8 = s8 + df[' "Amount"'][i]
+  if df[' "Solution"'][i] == 'Audit et conseil':
+    s9 = s9 + df[' "Amount"'][i]
+  if df[' "Solution"'][i] == 'SOC':
+    s10 = s10 + df[' "Amount"'][i]
+  if df[' "Solution"'][i] == 'Maintenance':
+    s11 = s11 + df[' "Amount"'][i]
+  if df[' "Solution"'][i] == 'DevOps':
+    s12 = s12 + df[' "Amount"'][i]
+  if df[' "Solution"'][i] == 'NaN':
+    s13 = s13 + df[' "Amount"'][i]
+df6 = pd.DataFrame([['Cloud', s1],['Postes de travail',s2],['Réseau', s3],['Régie INEOS', s4],['Sécurité', s5],['Firewalling',s6],['Régie Cyberforce', s7],['Sécurité Poste de Travail', s8],
+                   ['Audit et conseil', s9],['SOC',s10],['Maintenance', s11],['DevOps', s12],['NaN', s13]
+                   ], columns = ['Solution','Sales'])
+fig5 = px.bar(df6, x="Solution", y="Sales")
+st.plotly_chart(fig5)
 
 ####################################GRAPHES_PLOTTING############################
 
